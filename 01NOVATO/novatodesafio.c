5291710c69b3531456740bcb034e621ccceb018a
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 16:36:08 +0000

    Declaração das variáveis

---
+++ 01NOVATO/novatodesafio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("DESAFIO SUPER TRUNFO - NOVATO!\n");
+
+    // Declaração das variáveis 
+
+    char estado1, estado2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
