29ff5acb2627fe2e33c9e61a38418acd61f580f0
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 17:10:14 +0000

    Declaração das variáveis

---
+++ 03MESTRE/mestredesafio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("DESAFIO SUPER TRUNFO - MESTRE!\n");
+
+    // Declaração das variáveis 
+
+    char estado1, estado2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    unsigned long int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
+    float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
+    float pibPerCapitaCidade1, pibPerCapitaCidade2;
+    float superPoderCidade1, superPoderCidade2;
